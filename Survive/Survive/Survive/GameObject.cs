2c6c8347a5a9a570a7e4b1983200f1f6ef00c6d9
Rafael Moreyra <omgwtflmaobacon@gmail.com>
2013-03-15 16:47:38 -0400

    GameObject Class Added

---
+++ Survive/Survive/Survive/GameObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Survive
+{
+    abstract class GameObject
+    {
+        private int xPosition;
+        private int yPosition;
+    }
+}
