550627ba77e443299780f981ae5ad934537c7176
Virginia Hager <vrh4253@g.rit.edu>
2013-03-15 17:38:37 -0400

    Added Customizable Weapons

---
+++ Survive/Survive/Survive/WeaponCustomizable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Survive
+{
+    class WeaponCustomizable : Weapon
+    {
+        protected string name;
+        protected GunBarrel barrel;
+        protected GunBody body;
+        protected GunClip clip;
+        protected GunScope scope;
+        protected GunStock stock;
+    }
+}
