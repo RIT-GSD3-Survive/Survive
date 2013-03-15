852b92c7134e6c8d8a65840f1618a656311de1cd
Virginia Hager <vrh4253@g.rit.edu>
2013-03-15 17:35:36 -0400

    Added Healing/Ammo items

---
+++ Survive/Survive/Survive/AmmoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Survive
+{
+    class AmmoItem : Item
+    {
+        protected int amount;
+        protected string name;
+    }
+}
