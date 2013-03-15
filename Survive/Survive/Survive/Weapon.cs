aec72b1432855a85d8ee7393223ad00a8956feaf
Virginia Hager <vrh4253@g.rit.edu>
2013-03-15 17:14:00 -0400

    Weapon and Melee Weapon added

---
+++ Survive/Survive/Survive/Weapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Survive
+{
+    class Weapon : Item
+    {
+        private int baseAttackPower;
+        private int baseWeight;
+        private int baseAccuracy;
+    }
+}
