905728d7a5a21d98aedcce3e5e4692cb46d285ee
Chris Cheng <avaera8820@gmail.com>
2013-03-20 15:34:27 -0400

    The beginnings of the NBT Helper Code
    Will be used to help read maps and write player savedata

---
+++ Survive/Survive/Survive/NBTAssist/NBTTag.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Survive.NBTAssist {
+
+    public enum TagType {
+        
+    }
+
+    abstract class NBTTag {
+        private String name;
+
+        public NBTTag(String name) {
+            this.name = name;
+        }
+
+        public String Name {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+
+    class NBTByte : NBTTag {
+        
+    }
+
+    class NBTShort : NBTTag {
+        
+    }
+
+    class NBTInt : NBTTag {
+
+    }
+
+    class NBTLong : NBTTag {
+
+    }
+
+    class NBTFloat : NBTTag {
+
+    }
+
+    class NBTDouble : NBTTag {
+
+    }
+
+    class NBTByteArray : NBTTag {
+
+    }
+
+    class NBTString : NBTTag {
+
+    }
+
+    class NBTList : NBTTag {
+
+    }
+
+    class NBTCompound : NBTTag {
+
+    }
+
+    class NBTIntArray : NBTTag {
+
+    }
+}
