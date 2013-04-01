ed68d3be7a14ce13e37eeb90baf4a93bad884f7a
The Stick <avaera8820@gmail.com>
2013-04-01 15:28:02 -0400

    Couple more things...
    /Just/ in case VS was screaming at you for missing resources etc.

---
+++ Survive/MapEditor/MapEditor/Area.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MapEditor {
+    class Area {
+        private String name;
+
+        public static Texture2D cap, mid;
+
+        public String Name {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private struct Tile {
+            int x, y;
+
+            public override bool Equals(object obj) {
+                if(obj is Tile) {
+                    Tile test = (Tile)obj;
+                    return (test.x == this.x && test.y == this.y);
+                }
+                return false;
+            }
+
+            public Tile(int x, int y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        List<Tile> map = new List<Tile>();
+
+        public void AddTile(int x, int y) {
+            if(!map.Contains(new Tile(x, y))) {
+                map.Add(new Tile(x, y));
+            }
+        }
+
+        public void RemoveTile(int x, int y) {
+            if(map.Contains(new Tile(x, y))) {
+                map.Remove(new Tile(x, y));
+            }
+        }
+
+        public void DrawArea(SpriteBatch sb) {
+
+        }
+
+        public int DrawButton(SpriteBatch sb, SpriteFont sf, int x) {
+            int strLength = (int)Math.Ceiling(sf.MeasureString(name).X);
+
+            return 0;
+        }
+    }
+}
---
+++ Survive/MapEditor/MapEditor/Button.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace MapEditor {
+
+    class Button {
+        Texture2D tex;
+        Rectangle rect;
+
+        bool hold;
+
+        bool last = false;
+
+        public Button(Texture2D tex, bool holdButton, int x, int y, Click fn) {
+            this.tex = tex;
+            hold = holdButton;
+            rect = new Rectangle(x, y, tex.Width, tex.Height);
+            Clicked = fn;
+        }
+
+        public delegate void Click();
+
+        public Click Clicked;
+
+        public void CheckClicked(int x, int y) {
+            if(rect.Contains(new Point(x, y)) && this.Clicked != null) {
+                if(hold || !last) {
+                    this.Clicked();
+                    last = true;
+                }
+            } else {
+                last = false;
+            }
+        }
+
+        public void Draw(SpriteBatch sb) {
+            sb.Draw(tex, rect, Color.White);
+        }
+    }
+}
