44a9b48c0480680a900db0c65cb46b4efcfaabbe
The Stick <avaera8820@gmail.com>
2013-04-12 16:29:06 -0400

    Adding in a smaller Courier SpriteFont
    Courier New, 8pt.  Used for Namecards, etcetera.  Just for things where
    18pt Courier New wouldn't work.

--- Survive/Survive/Survive/Resources.cs
+++ Survive/Survive/Survive/Resources.cs
@@ -7,12 +7,16 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Survive {
     class Resources {
-        private static SpriteFont courier;
+        private static SpriteFont courier, courierSmall;
 
         private static Texture2D tiles;
 
         public static SpriteFont Courier {
             get { return courier; }
+        }
+
+        public static SpriteFont CourierSmall {
+            get { return courierSmall; }
         }
 
         public static Texture2D Tiles {
@@ -21,6 +25,7 @@ namespace Survive {
 
         public static void LoadRes(ContentManager cm) {
             courier = cm.Load<SpriteFont>("Courier");
+            courierSmall = cm.Load<SpriteFont>("CourierSmall");
             tiles = cm.Load<Texture2D>("Tiles");
         }
     }
