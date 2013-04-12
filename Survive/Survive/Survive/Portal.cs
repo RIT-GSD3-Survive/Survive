b6bf379a7676567b5be696316b3de9ae8f49f80b
The Stick <avaera8820@gmail.com>
2013-04-12 16:21:37 -0400

    Adding in a Portal empty class

---
+++ Survive/Survive/Survive/Portal.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Survive {
+    class Portal : Terrain {
+        protected Area linkTo;
+        protected int linkX, linkY;
+    }
+}
